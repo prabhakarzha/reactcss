05f4a68863a8ccc5d2c19256ed8b782a161ff4ba
prabhakar kumar <70058887+prabhakarzha@users.noreply.github.com>
2021-04-21 11:10:30 +0530

    cnanges

--- src/App.js
+++ src/App.js
@@ -2,43 +2,153 @@ import React from "react";
 import { Field ,Form } from "react-final-form";
 
 import "./App.css";
+const onSubmit =values => window.alert(JSON.stringify(values,0,2));
+
 
 const App =()=>(
-  <Form onSubmit ={()=>{}}
-  render ={({handleSubmit,values}) =>(
-    <form>
-      <div>
-        <label htmlFor="firstname">First Name</label>
-        <Field
-              name ="firstName"
-              component ="input"
-              type="text"
-              placeholder ="First Name"/>
-              
-      </div>
+  <Form 
+  onSubmit ={onSubmit}
+  validate ={values=>{
+    const errors={};
 
-      <div>
-        <label htmlFor="lastname">Last Name</label>
-        <Field
-              name ="lastName"
-              component ="input"
-              type="text"
-              placeholder ="Last Name"/>
+    if (!values.password){
+      errors.password ='password is empty';
+
+    }
+    else if(values.password.length < 8){
+      errors.password ='Password is too short'
+    }
+    if (!values.lastname){
+      errors.lastname ='lastname is empty';
+
+    }
+
+    else if(values.lastname.length < 5){
+      errors.lastname ='lastname is too short'
+    }
+    if (!values.firstname){
+      errors.firstname ='firstname is empty';
+
+    }
+
+    else if(values.firstname.length < 5){
+      errors.firstname ='firstname is too short'
+    }
+   
+    if (!values.contact){
+      errors.contact ='Contact is empty';
+
+    }
+
+    else if(values.contact.length < 10){
+      errors.contact ='contact is too short'
+    }
+  
+    console.log(errors);
+    return errors;
+
+  }}
+  render ={({handleSubmit,values,submitting}) =>(
+    <div className="contact">
+    <form onSubmit ={handleSubmit}>
+      < div className="wrapper" >
+
+
+        
+      <Field name ='firstname'>
+                {({input,meta}) =>{
+                  return( 
+                  <div>
+                    <div>
+                      <label htmlFor ='firstname'>First Name</label>
+                      <input {...input} type='text' placeholder ='First Name'/>
+                    </div>
+                    <div className ='error'>
+                    {meta.error && meta.touched &&(
+                      <span>{meta.error}</span>
+                    )}
+                    </div>
+                  </div>
+                  )
+                }}
+              </Field>
+
+     
+       
+
+              <Field name ='password'>
+                {({input,meta}) =>{
+                  return( 
+                  <div>
+                    <div>
+                      <label htmlFor ='password'>Password</label>
+                      <input {...input} type='text' placeholder ='Password'/>
+                    </div>
+                    <div className ='error'>
+                    {meta.error && meta.touched &&(
+                      <span>{meta.error}</span>
+                    )}
+                    </div>
+                  </div>
+                  )
+                }}
+              </Field>
+               
               
-      </div>
+     
+
+     
+        
+        <Field name ='lastname'>
+                {({input,meta}) =>{
+                  return( 
+                  <div>
+                    <div>
+                      <label htmlFor ='lastname'>Last Name</label>
+                      <input {...input} type='text' placeholder ='lastname'/>
+                    </div>
+                    <div className ='error'>
+                    {meta.error && meta.touched &&(
+                      <span>{meta.error}</span>
+                    )}
+                    </div>
+                  </div>
+                  )
+                }}
+              </Field>
+             
+              
+      
+      
 
       
-      <div >
-        <label htmlFor="email">Email</label>
-        <Field
-              name ="email"
-              component ="input"
-              type="email"
-              placeholder ="Email"/>
+      
+       
+        <Field name ='email'>
+                {({input,meta}) =>{
+                  return( 
+                  <div>
+                    <div>
+                      <label htmlFor ='email'>Email</label>
+                      <input {...input} type='text' placeholder ='email'  />
+                    </div>
+                    <div className ='error'>
+                    {meta.error && meta.touched &&(
+                      <span>{meta.error}</span>
+                    )}
+                    </div>
+                  </div>
+                  )
+                }}
+              </Field>
+             
               
-      </div>
+              
+      
 
-      <div >
+   
+        
+        
         <label htmlFor="password">Password</label>
         <Field
               name ="password"
@@ -46,22 +156,42 @@ const App =()=>(
               type="password"
               placeholder ="Password"/>
               
-      </div>
      
-      <div >
-        <label htmlFor="contact">Contact</label>
-        <Field
-              name ="contact"
-              component ="input"
-              type="text"
-              placeholder ="Contact"/>
-              
-      </div>
-              <button type="submit">Submit</button>
-            
+     
+      
       
      
+        <Field name ='contact'>
+                {({input,meta}) =>{
+                  return( 
+                  <div>
+                    <div>
+                      <label htmlFor ='contact'>Contact</label>
+                      <input {...input} type='text' placeholder ='contact'/>
+                    </div>
+                    <div className ='error'>
+                    {meta.error && meta.touched &&(
+                      <span>{meta.error}</span>
+                    )}
+                    </div>
+                  </div>
+                  )
+                }}
+              </Field>
+              
+              
+      
+      <div className="full">
+              <button type="submit">Submit</button>
+              </div>
+              </div> 
+             
+            
+      
+               
     </form>
+    
+    </div>
   )}
   />
 )
